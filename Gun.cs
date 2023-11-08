d1281907a766439c76c96884800822b8a8b6677a
Khoa <130851064+Khoa-thich-code@users.noreply.github.com>
2023-11-09 01:20:30 +0700

    Update Gun.cs

--- Gun.cs
+++ Gun.cs
@@ -1,11 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class Gun : MonoBehaviour
 {
     public Ammo ammo;
-    public int GetRoundLeft()
-    {
-        return ammo.roundsLeft;
-    }
-}                                                                                                                                                                                                  
+}
